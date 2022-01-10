c30f4429194518fe0f33352cb97678ada8c65640
NotCreative001 <55113844+NotCreative001@users.noreply.github.com>
2022-01-09 20:10:58 -0500

    parallelized qsort
    the quick sort is now parallelized. when the depth is less than 2 when it divides the vector in two it also spawns a thread to solve the other path (can create up to 4 threads). all of the threads run in parallel to solve their parts of the vector. the results are merged together using reduction for efficiency.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     for i in 1..101 {
         vector[100-(i as usize)]=i;
     }
-    println!("arr{:?}",vector);
+    //println!("inital {:?}",vector);
     let sorted =qsort(vector, 0);
     for val in sorted {
         print!("{},",val);
@@ -18,19 +18,13 @@ fn qsort(vector: Vec<i32>, num_depth: i8)->Vec<i32> {
         return vector;
     }
 
+    //set up left side vector
     let mut left_vect =vec![0i32; vector.len()/2];
     for i in 0..vector.len()/2 {
         left_vect[i] = vector[i];
     }
-    //sort both sides
-    
-    let left;
-    if num_depth < 2 {
-        println!("created thread");
-        left = qsort(left_vect, num_depth +1);
-    } else{
-        left = qsort(left_vect, num_depth +1);
-    }
+
+    //set up right side vector
     let size;
     if vector.len()%2 ==1 {
         size =vector.len()/2 +1;
@@ -41,11 +35,26 @@ fn qsort(vector: Vec<i32>, num_depth: i8)->Vec<i32> {
     for i in 0..size  {
         right_vect[i] = vector[i+vector.len()/2];
     }
-    let right = qsort(right_vect, num_depth +1);
+
+    //sort both sides
+    let left;
+    let right;
+    let handler;
+    //if the depth is low enough spawn a new thread for one of the halfs
+    if num_depth < 2 {
+        handler =std::thread::spawn(move || {qsort(left_vect, num_depth +1)}); //spawn new thread
+        right = qsort(right_vect, num_depth +1); //do right side
+        left = handler.join().unwrap(); //join the left side
+        
+    } else{
+        left = qsort(left_vect, num_depth +1); //do the left side
+        right = qsort(right_vect, num_depth +1); //do the right side
+    }
+    
+    
 
     //form this into a new list
     let mut sorted = vec![0i32;vector.len()];
-
     let mut i =0;
     let mut j=0;
     let mut k=0;
@@ -61,6 +70,7 @@ fn qsort(vector: Vec<i32>, num_depth: i8)->Vec<i32> {
         }
     }
 
+    //puts any left over unsorted stuff into the sorted
     if i ==left.len() {
         for l in j..right.len() {
             sorted[k]=right[l];
@@ -72,7 +82,6 @@ fn qsort(vector: Vec<i32>, num_depth: i8)->Vec<i32> {
             k +=1;
         }
     }
-
 
     return sorted;
 }
